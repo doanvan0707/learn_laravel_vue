d05342d510edca71a1c8419fabf24426ed0ebe70
Doãn Văn Vân <doanvanvandvv@gmail.com>
2019-11-09 22:29:56 +0700

    - update

--- spa-laravel/app/Http/Controllers/SinglePageController.php
+++ spa-laravel/app/Http/Controllers/SinglePageController.php
@@ -1,17 +1,13 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\FileUpload;
-
 class SinglePageController extends Controller
 {
     public function index() 
     {
         return view('app');
     }
-
     public function store(Request $request)
     {
         if ($request->get('image'))
@@ -20,14 +16,11 @@ class SinglePageController extends Controller
             $name = time().'.'. explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];
             \Image::make($request->get('umage'))->save(public_path('images/').$name);
         }
-
         $image = new FileUpload();
         $image->image_name = $name;
         $image->save();
-
         return response()->json(['success' => 'You have successfully uploaded an image'], 200);
     }
-
     public function testAxios() 
     {
         return response()->json([
